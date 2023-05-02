de167d267663eb9abb42d68f9545daedd2794882
vanshika308 <132396507+vanshika308@users.noreply.github.com>
2023-05-03 00:44:42 +0530

    Add files via upload

---
+++ javascript/index.js
@@ -0,0 +1,12 @@
+function display(){
+     let Name=document.getElementById('name');
+     console.log(Name.value);
+     let Email=document.getElementById('email');
+     console.log(Email.value);
+     let Phone=document.getElementById('phone');
+     console.log(Phone.value);
+     let date=document.getElementById('date');
+     console.log(date.value);
+     let time=document.getElementById('time');
+     console.log(time.value);
+}
